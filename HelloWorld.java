7de2399d32d2dc3f05a7425ee0de91692e8887fa
vijayadinesh <57358828+vijayadinesh@users.noreply.github.com>
2020-03-05 12:43:43 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,7 +3,7 @@ public class HelloWorld {
 public static void main (String [] args) {
 	System.out.println("------- ***** Hellow World & Welcome to jenkins Training ***** ------- ");
 	System.out.println("------- ***** Made Some Changes ***** ------- ");
-	System.out.println("------- ***** Checking WebHooks ***** ------- ");
+	System.out.println("------- ***** Checking WebHooks , Looks Good ***** ------- ");
 }
 
 }
